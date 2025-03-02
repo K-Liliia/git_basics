c0b7f76e4f182c93cafef5b38232d3239fb9ee45
Liliia Kozachuk <vodoliy96@gmail.com>
2025-03-02 22:31:26 +0200

    Updated code due to comments

--- hometask_7_ts/functions_folder/arrow-function.ts
+++ hometask_7_ts/functions_folder/arrow-function.ts
@@ -1,12 +1,14 @@
-const calculateSumOfElemOrConcatIt = (arrayOfValues: string[] | number[]): (string | number)[] => {
-    let sumNumbers = 0;
-    let sumStrings = '';
-    for (const elem of arrayOfValues) {
-        typeof (elem) === 'number' ? sumNumbers += elem : sumStrings += elem;
+const calculateSumOfElemOrConcatIt = (arrayOfValues: string[] | number[]): (string | number) => {
+    if (arrayOfValues.length === 0) {
+        return 'Array is empty';
     }
-    return [sumNumbers, sumStrings];
 
+    if (typeof arrayOfValues[0] === 'number') {
+        return (arrayOfValues as number[]).reduce((sum, num) => sum + num, 0);
+    } else {
+        return (arrayOfValues as string[]).reduce((result, str) => result + str, '');
+    }
 };
 
-console.log(calculateSumOfElemOrConcatIt([1, 2, 3, 4, 5, 6, 7]));
-console.log(calculateSumOfElemOrConcatIt(['new', 'old', 'up-to-date', 'old-fashioned']));
+console.log('Sum of numbers is: ' + calculateSumOfElemOrConcatIt([1, 2, 3, 4, 5, 6, 7]));
+console.log('Sum of strings is: ' + calculateSumOfElemOrConcatIt(['new', 'old', 'up-to-date', 'old-fashioned']));
--- hometask_7_ts/functions_folder/functions.ts
+++ hometask_7_ts/functions_folder/functions.ts
@@ -1,18 +1,14 @@
-function calculateSumOrConcat(arrayOfValues: string[] | number[]): (string | number)[] {
-    let sumNumbers = 0;
-    let sumStrings = '';
-
-    for (const elem of arrayOfValues) {
-        if (typeof (elem) === 'number') {
-            sumNumbers += elem;
-        } else {
-            sumStrings += elem;
-        }
+function calculateSumOrConcat(arrayOfValues: string[] | number[]): string | number {
+    if (arrayOfValues.length === 0) {
+        return 'Array is empty';
     }
-    return [sumNumbers, sumStrings];
+
+    if (typeof arrayOfValues[0] === 'number') {
+        return (arrayOfValues as number[]).reduce((sum, num) => sum + num, 0);
+    } else {
+        return (arrayOfValues as string[]).reduce((result, str) => result + str, '');
+    }
 }
 
 console.log('Sum of numbers is: ' + calculateSumOrConcat([4, 5, 6, 8, 22, 33, 44]));
 console.log('Sum of strings is: ' + calculateSumOrConcat(['red raspberry', 'yellow lemon', 'green lime', 'purple plum', 'black blackberry']));
-
-
